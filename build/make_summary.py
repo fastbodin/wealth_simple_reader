2d9bb6a3212009f4ee15d7f88d14326445af7ffc
fastbodin <aslobodin@proton.me>
2024-05-07 12:23:58 -0700

    added function to determine annually compounded rate of return

--- build/make_summary.py
+++ build/make_summary.py
@@ -1,4 +1,42 @@
 import pandas as pd
+import numpy as np
+from scipy.optimize import minimize_scalar
+
+def calculate_annual_compounded_rate(df, total_portfolio_value):
+    # Convert 'Date' column to datetime
+    # taking the 'Start' of statement period to be the deposit date, even
+    # though its between 'Start' and 'End'
+    df['Date'] = pd.to_datetime(df['Start'])
+    
+    # Sort df by date
+    df = df.sort_values(by='Date')
+    
+    # Calculate the time difference in years
+    # (most recent date) - (deposit date) <- converted to years
+    df['Years_Invested'] = ((df['Date'].max() - df['Date']).dt.days)/365
+    
+    # Define the objective function to minimize
+    def objective_function(rate):
+        # taking absolute values ensures that the objective_function is is >= 0
+        # .mul is element-wise multiplication
+        # this looks like (deposit value)*(rate of return)^(years deposit is invested)
+        # summed over all deposits
+        return np.abs(df['Deposits'].mul((1 + rate)**(df['Years_Invested'])).sum() 
+                      - total_portfolio_value)
+
+    
+    # Find the rate that minimizes the difference between the sum of compounded 
+    # deposits and the total portfolio value. NOTE, I am assuming here that
+    # your rate is between 0 and 100% (compounded annually), if you have a
+    # greater performance than this, change the bounds to (0,x) where x is your
+    # maximum possible compounded rate of return
+    result = minimize_scalar(objective_function, method='bounded', bounds=(0, 1))
+    
+    # The result x is the annual compounded rate
+    annual_compounded_rate = result.x*100
+    
+    return annual_compounded_rate
+
 
 def main():
     holdings_df = pd.read_csv("data/holdings.csv")
@@ -21,9 +59,10 @@ def main():
     holdings_df.rename(columns={'total_quantity': 'Quantity'}, inplace=True)
 
 
-    print("---------------------- {} HOLDINGS ----------------------".format(recent_statement))
+    print("\n---------------------- {} HOLDINGS ----------------------".format(recent_statement))
     print(holdings_df[["Ticker", "Return (%)", "Return ($)", "% of portfolio", "Quantity", "Value ($)"]].to_string(index=False))
-    print("-----------------------------------------------------------------")
+    print("Cash: ${:.2f}".format(cash_in_df[cash_in_df.End == recent_statement].Cash.values[0]))
+    print("-----------------------------------------------------------------\n")
 
 
     sector_df = holdings_df.groupby(by = "sector")['Value ($)'].sum().reset_index()
@@ -33,7 +72,7 @@ def main():
 
     print("---------------------- {} SECTOR  -----------------------".format(recent_statement))
     print(sector_df[["Sector", "% of portfolio"]].to_string(index=False))
-    print("----------------------------------------------------------------")
+    print("----------------------------------------------------------------\n")
 
 
     region_df = holdings_df.groupby(by = "region")['Value ($)'].sum().reset_index()
@@ -43,14 +82,22 @@ def main():
 
     print("---------------------- {} REGION  ----------------------".format(recent_statement))
     print(region_df[["Region", "% of portfolio"]].to_string(index=False))
-    print("----------------------------------------------------------------")
+    print("\nDesired: 25% CAD, 35% US, 15% INT, 25% Fixed")
+    print("----------------------------------------------------------------\n")
 
-    print("---------------------- TOTAL SUMMARY  ----------------------")
-    print("Value: ${:.2f}".format(port_value))
-    print("Cash: ${:.2f}".format(cash_in_df[cash_in_df.End == recent_statement].Cash.values[0]))
+
+    print("---------------------- PERFORMACE SUMMARY ----------------------")
     print("Deposits: ${:.2f}".format(cash_in_df.Deposits.sum()))
+    print("Portfolio Value: ${:.2f}".format(port_value))
+    print("Return ($): ${:.2f}".format(port_value-cash_in_df.Deposits.sum()))
+    print("Return (%): {:.2f}%".format((port_value/cash_in_df.Deposits.sum() -1)*100))
+    print("Annual compounded return (%): {:.2f}%".format(
+          calculate_annual_compounded_rate(cash_in_df, port_value)))
+    print("-----------------------------------------------------------------\n")
+
+    print("---------------------- DIVIDENDS AND TAXES  ----------------------")
     print("Dividends: ${:.2f}".format(cash_in_df.Dividends.sum()))
     print("Taxes: ${:.2f}".format(cash_out_df.Taxes.sum()))
-    print("------------------------------------------------------------")
+    print("------------------------------------------------------------------")
 
 main()
